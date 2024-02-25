58a628745f071b975e9b9ba1eaa06f7ec56a76c1
Wellington2708 <wellingtondevelopercontact@gmail.com>
2024-02-25 11:34:21 +0000

    finish

---
+++ src/components/Filtro/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export default function Filtro(){
+    return(
+    <section className="container-filtro">
+        <ul>
+            <li>Programação</li>
+            <li>Front-end</li>
+        </ul>
+        <button>Limpar tudo</button>
+    </section>
+    )
+}
